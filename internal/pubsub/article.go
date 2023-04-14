17244b8b244b9942098fe1081157cf9ca7aa59d9
Igor Agapie <igoragapie@gmail.com>
2023-04-14 15:13:48 +0200

    add relative pub date

--- internal/pubsub/article.go
+++ internal/pubsub/article.go
@@ -8,29 +8,29 @@ import (
 )
 
 type Article struct {
-	ID              uuid.UUID `json:"id,omitempty"`
-	SiteID          uuid.UUID `json:"site_id,omitempty"`
-	Lang            string    `json:"lang,omitempty"`
-	Title           string    `json:"title,omitempty"`
-	ShortDesc       string    `json:"short_desc,omitempty"`
-	LongDesc        string    `json:"long_desc,omitempty"`
-	Link            string    `json:"link,omitempty"`
-	Image           string    `json:"image,omitempty"`
-	PubDate         time.Time `json:"pub_date,omitempty"`
-	RelativePubDate string    `json:"relative_pub_date,omitempty"`
-	Categories      []string  `json:"categories,omitempty"`
+	ID         uuid.UUID `json:"id,omitempty"`
+	SiteID     uuid.UUID `json:"site_id,omitempty"`
+	Lang       string    `json:"lang,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	ShortDesc  string    `json:"short_desc,omitempty"`
+	LongDesc   string    `json:"long_desc,omitempty"`
+	Link       string    `json:"link,omitempty"`
+	Image      string    `json:"image,omitempty"`
+	PubDate    time.Time `json:"pub_date,omitempty"`
+	PubDiff    string    `json:"pub_diff,omitempty"`
+	Categories []string  `json:"categories,omitempty"`
 }
 
 func FromEntity(e *entity.Article) Article {
 	a := Article{
-		ID:              e.ID,
-		SiteID:          e.SiteID,
-		Lang:            e.Lang,
-		Title:           e.Title,
-		Link:            e.Link,
-		Image:           e.FirstMedia(entity.ImageType).URL,
-		PubDate:         e.PubDate,
-		RelativePubDate: timediff.TimeDiff(e.PubDate, timediff.WithStartTime(time.Now().UTC())),
+		ID:      e.ID,
+		SiteID:  e.SiteID,
+		Lang:    e.Lang,
+		Title:   e.Title,
+		Link:    e.Link,
+		Image:   e.FirstMedia(entity.ImageType).URL,
+		PubDate: e.PubDate,
+		PubDiff: timediff.TimeDiff(e.PubDate, timediff.WithStartTime(time.Now().UTC())),
 	}
 
 	if e.ShortDesc != nil {
